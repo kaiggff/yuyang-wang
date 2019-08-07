e3e91fbb59a4f6926fe6dbf0dc25ad6dbed6a501
kaiggff <52937056+kaiggff@users.noreply.github.com>
2019-08-07 18:28:05 +0200

    Add files via upload

---
+++ scalarfield.py
@@ -0,0 +1,20 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+kx = np.arange(-50, 50, 0.1)
+e = math.e
+
+def s(x):
+	
+	return 1 / (1 + e ** -x)
+	
+plt.ylim(0, 3)
+plt.xlabel('x', fontsize = 15)
+plt.ylabel('$\phi$', fontsize = 15)
+#plt.title("scalar field $\phi$ v.s. x")
+plt.plot(kx, s(kx - 20)+s(-kx - 20)+1, color='r')
+#plt.plot(kx, s(kx), color='black')
+plt.grid(True)
+plt.show()
